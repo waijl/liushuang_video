ce82f3a05fce618bfcb7bc756967df721421ae3b
waijl <3031083347@qq.com>
2021-03-19 15:30:07 +0800

    PermissionUtils finish

--- app/src/main/java/com/liushuang/liushuang_video/home/NovelFragment.java
+++ app/src/main/java/com/liushuang/liushuang_video/home/NovelFragment.java
@@ -1,11 +1,18 @@
 package com.liushuang.liushuang_video.home;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,19 +21,24 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.liushuang.liushuang_video.AppManager;
 import com.liushuang.liushuang_video.R;
 import com.liushuang.liushuang_video.model.book.BookResult;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.FileAsyncHttpResponseHandler;
 
+import java.io.File;
 import java.util.List;
 
-import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.entity.mime.Header;
+
 
 public class NovelFragment extends Fragment {
 
+    private static final String TAG = "NovelFragment";
     private ListView mListView;
     private List<BookResult.Book> mBookList;
     private AsyncHttpClient mClient;
@@ -44,6 +56,11 @@ public class NovelFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         initView();
         initData();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())){
+                initPermission();
+            }
+        }
     }
 
     private void initView() {
@@ -62,7 +79,12 @@ public class NovelFragment extends Fragment {
             }
 
             @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+            public void onFinish() {
+                super.onFinish();
+            }
+
+            @Override
+            public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody) {
                 String result = new String(responseBody);
                 BookResult bookResult = AppManager.getGson().fromJson(result, BookResult.class);
                 mBookList = bookResult.getData();
@@ -70,18 +92,34 @@ public class NovelFragment extends Fragment {
             }
 
             @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+            public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody, Throwable error) {
 
-            }
-
-            @Override
-            public void onFinish() {
-                super.onFinish();
             }
         });
+
     }
 
-    class BookListAdapter extends BaseAdapter{
+    private void initPermission() {
+        int permission1 = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int permission2 = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (permission1 != PackageManager.PERMISSION_GRANTED && permission2 != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
+
+        /*if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission_group.STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission_group.STORAGE}, 1);
+        }*/
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            // TODO: 2021/3/18
+        }
+    }
+
+    class BookListAdapter extends BaseAdapter {
 
         @Override
         public int getCount() {
@@ -103,23 +141,66 @@ public class NovelFragment extends Fragment {
 
             BookResult.Book book = (BookResult.Book) getItem(position);
             ViewHolder holder = null;
-            if (convertView == null){
+            if (convertView == null) {
                 convertView = getLayoutInflater().inflate(R.layout.item_list_book, parent, false);
                 holder = new ViewHolder(convertView);
                 convertView.setTag(holder);
-            }else {
+            } else {
                 holder = (ViewHolder) convertView.getTag();
             }
 
             holder.mNameTextView.setText(book.getBookname());
+
+//            Android10以上已经过时
+//            String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/imooc/" + book.getBookname() + ".txt";
+//            String path = getActivity().getFilesDir().getAbsolutePath() + "/book/" + book.getBookname() + ".txt";
+//            File file = new File(getActivity().getExternalFilesDir(null), "/book/" + book.getBookname() + ".txt");
+
+//            /storage/emulated/0/Android/data/yourPackageName/files/novel/bookname.txt
+            String path = getActivity().getExternalFilesDir(null).getAbsolutePath() + "/novel/" + book.getBookname() + ".txt";
+//            Log.d(TAG, "getView: path = " + path);
+            File file = new File(path);
+            holder.mButton.setText(file.exists() ? "打开" : "下载");
+            ViewHolder finalHolder = holder;
+            holder.mButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (file.exists()) {
+                        // TODO: 2021/3/18
+                    } else {
+                        mClient.addHeader("Accept-Encoding", "identity");
+                        mClient.get(book.getBookfile(), new FileAsyncHttpResponseHandler(file) {
+                            @Override
+                            public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, Throwable throwable, File file) {
+                                finalHolder.mButton.setText("下载");
+                                Toast.makeText(getActivity(), "下载失败，请重新下载", Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, File file) {
+                                finalHolder.mButton.setText("打开");
+
+                            }
+
+                            @Override
+                            public void onProgress(long bytesWritten, long totalSize) {
+                                super.onProgress(bytesWritten, totalSize);
+                                finalHolder.mButton.setText(String.valueOf(bytesWritten * 100 / totalSize) + "%");
+                            }
+
+                        });
+                    }
+                }
+            });
+
             return convertView;
         }
 
-        class ViewHolder{
+        class ViewHolder {
             public TextView mNameTextView;
             public Button mButton;
 
-            public ViewHolder(View view){
+            public ViewHolder(View view) {
                 mNameTextView = view.findViewById(R.id.id_tv_bookName);
                 mButton = view.findViewById(R.id.id_btn_read);
             }
