8987d512b5e407b381b8a6a9475321c834c9e92f
waijl <3031083347@qq.com>
2021-03-23 10:46:30 +0800

    Login and Register majority finish

--- app/src/main/java/com/liushuang/liushuang_video/login/LoginActivity.java
+++ app/src/main/java/com/liushuang/liushuang_video/login/LoginActivity.java
@@ -1,17 +1,17 @@
 package com.liushuang.liushuang_video.login;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
+import android.content.SharedPreferences;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.liushuang.liushuang_video.R;
 import com.liushuang.liushuang_video.base.BaseActivity;
-import com.liushuang.liushuang_video.home.HomeActivity;
 
 public class LoginActivity extends BaseActivity {
 
@@ -20,6 +20,12 @@ public class LoginActivity extends BaseActivity {
     private EditText mPassword;
     private TextView mLogin;
     private TextView mRegister;
+    private SharedPreferences mSharedPreferences;
+
+    private static final String KEY_USERNAME = "key_username";
+    private static final String KEY_PASSWORD = "password";
+    private String username;
+    private String password;
 
     @Override
     protected int getLayoutId() {
@@ -28,6 +34,8 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     protected void initView() {
+
+        initIntent(getIntent());
         mClose = bindViewId(R.id.id_iv_close);
         mUsername = bindViewId(R.id.edt_username);
         mPassword = bindViewId(R.id.edt_pwd);
@@ -37,13 +45,19 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     protected void initData() {
+        mSharedPreferences = getSharedPreferences(RegisterActivity.USER_INFO_PREFENCES, MODE_PRIVATE);
+        username = mSharedPreferences.getString(RegisterActivity.USER_NAME, username);
+        password = mSharedPreferences.getString(RegisterActivity.PASSWORD, password);
+        if (username != null && password != null) {
+            mUsername.setText(username);
+            mPassword.setText(password);
+        }
+
+        initEvent();
+    }
+
+    private void initEvent() {
         mClose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-        mLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
@@ -57,5 +71,49 @@ public class LoginActivity extends BaseActivity {
                 startActivity(intent);
             }
         });
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)){
+                    Toast.makeText(LoginActivity.this, "账号或密码不能为空", Toast.LENGTH_SHORT).show();
+                    return;
+                }else {
+                    finish();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        initIntent(intent);
+    }
+
+    private void initIntent(Intent intent) {
+        setIntent(intent);
+        Intent intent1 = getIntent();
+        if (intent1 != null) {
+            username = intent1.getStringExtra(KEY_USERNAME);
+            password = intent1.getStringExtra(KEY_PASSWORD);
+
+            if (username != null) {
+                mUsername.setText(username);
+            }
+
+            if (password != null) {
+                mPassword.setText(password);
+            }
+        }
+    }
+
+    public static void launch(Context context, String registerName, String password){
+        Intent intent = new Intent(context, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra(KEY_USERNAME, registerName);
+        intent.putExtra(KEY_PASSWORD, password);
+        context.startActivity(intent);
+
     }
 }
--- app/src/main/java/com/liushuang/liushuang_video/login/RegisterActivity.java
+++ app/src/main/java/com/liushuang/liushuang_video/login/RegisterActivity.java
@@ -3,11 +3,14 @@ package com.liushuang.liushuang_video.login;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.liushuang.liushuang_video.R;
 import com.liushuang.liushuang_video.base.BaseActivity;
@@ -18,6 +21,11 @@ public class RegisterActivity extends BaseActivity {
     private EditText mPassword;
     private EditText mRePassword;
     private TextView mBtnRegister;
+    private SharedPreferences mSharedPreferences;
+
+    public static final String USER_INFO_PREFENCES = "userinfo";
+    public static final String USER_NAME = "username";
+    public static final String PASSWORD = "password";
 
     @Override
     protected int getLayoutId() {
@@ -41,7 +49,28 @@ public class RegisterActivity extends BaseActivity {
         mBtnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String registerName = mRegisterName.getText().toString();
+                String password = mPassword.getText().toString();
+                String rePassword = mRePassword.getText().toString();
 
+                if (TextUtils.isEmpty(registerName) || TextUtils.isEmpty(password) || TextUtils.isEmpty(rePassword)){
+                    Toast.makeText(RegisterActivity.this, "账号或密码不能为空", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (!password.equals(rePassword)){
+                    Toast.makeText(RegisterActivity.this, "两次输入密码不一致", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mSharedPreferences = getSharedPreferences(USER_INFO_PREFENCES, MODE_PRIVATE);
+                SharedPreferences.Editor edit = mSharedPreferences.edit();
+                edit.putString(USER_NAME, registerName);
+                edit.putString(PASSWORD, password);
+                edit.commit();
+
+                LoginActivity.launch(RegisterActivity.this, registerName, password);
+                finish();
             }
         });
     }
